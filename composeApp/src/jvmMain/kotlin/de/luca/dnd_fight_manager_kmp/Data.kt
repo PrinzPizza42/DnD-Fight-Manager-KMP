d73614cb9d92f262102bd696a0288510dcfd83a1
PrinzPizza42 <125275369+PrinzPizza42@users.noreply.github.com>
2026-02-04 14:45:52 +0100

    Changed style of save and load overlays

--- composeApp/src/jvmMain/kotlin/de/luca/dnd_fight_manager_kmp/Data.kt
+++ composeApp/src/jvmMain/kotlin/de/luca/dnd_fight_manager_kmp/Data.kt
@@ -8,15 +8,14 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.onClick
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
